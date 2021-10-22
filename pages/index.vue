f7f1dc1ef6a19639f9a003a05f0bb14eaffb27bd
Heranca <heranca.kamalo@gmail.com>
2021-10-22 13:00:34 +0200

    init home page && login on going

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,18 @@
-<template></template>
+
+
+<template>
+  <div>
+    <v-app-bar dark color="pink">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Troov App</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+  </div>
+  
+</template>
 
 <script>
 export default {};
