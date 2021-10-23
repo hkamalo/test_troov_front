4fcbbc121c871303039bfd22228f729b8f82214d
Heranca <heranca.kamalo@gmail.com>
2021-10-23 14:31:50 +0200

    add logo && logout button && add object button

--- pages/home.vue
+++ pages/home.vue
@@ -1,37 +1,17 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-container class="py-0 fill-height">
-        <v-avatar
-          class="mr-10"
-          color="grey darken-1"
-          size="32"
-        ></v-avatar>
+    <v-app-bar app color="white" flat class="app-bar">
+      <div class="app-menu">
+        <div>
+          <img src="../static/logo-dark.png" />
 
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          text
-        >
-          {{ link }}
-        </v-btn>
+          <v-btn v-for="link in links" :key="link" text>
+            {{ link }}
+          </v-btn>
+        </div>
 
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
+        <v-btn>Logout</v-btn>
+      </div>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
@@ -40,39 +20,25 @@
           <v-col cols="2">
             <v-sheet rounded="lg">
               <v-list color="transparent">
-                <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  link
-                >
+                <v-list-item v-for="item in items" :key="item" link>
                   <v-list-item-content>
-                    <v-list-item-title>
-                      List Item {{ n }}
-                    </v-list-item-title>
+                    <v-list-item-title>{{ item }} </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
 
                 <v-divider class="my-2"></v-divider>
 
-                <v-list-item
-                  link
-                  color="grey lighten-4"
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
+                <v-list-item link color="grey lighten-4">
+                  <v-btn>
+                    Ajoutez un object
+                  </v-btn>
                 </v-list-item>
               </v-list>
             </v-sheet>
           </v-col>
 
           <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
+            <v-sheet min-height="70vh" rounded="lg">
               <!--  -->
             </v-sheet>
           </v-col>
@@ -83,13 +49,24 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      links: [
-        'Profile',
-        'Object',
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    links: ["Profile", "Object"],
+    items: ["object 1", "object 2", "object 3", "object 4", "object 5"],
+  }),
+};
 </script>
- 
+
+<style scoped>
+.app-bar {
+  width: 100vw;
+}
+
+.app-menu {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
