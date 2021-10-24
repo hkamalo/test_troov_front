d22be3c9293dcea64544164c8ca676796796a6a9
Heranca <heranca.kamalo@gmail.com>
2021-10-24 23:38:19 +0200

    configure form boject

--- components/CreateObject.vue
+++ components/CreateObject.vue
@@ -6,66 +6,34 @@ vue
         <v-form>
           <v-text-field
             id="name"
-            label="a"
-            name="name"
-            prepend-icon="mdi-account"
-            type="text"
-          ></v-text-field>
-          <v-text-field
-            id="lastname"
             label="Nom"
             name="name"
-            prepend-icon="mdi-account-box-outline"
             type="text"
           ></v-text-field>
           <v-text-field
-            id="adresse"
+            id="description"
+            label="Description"
+            name="description"
+            type="text"
+          ></v-text-field>
+          <v-text-field
+            id="adress"
             label="Adresse"
-            name="adresse"
-            prepend-icon="mdi-map"
+            name="adress"
             type="text"
           ></v-text-field>
           <v-text-field
-            id="postcode"
-            label="Code postal"
-            name="postcode"
-            prepend-icon="mdi-map"
+            id="date"
+            label="Date"
+            name="date"
             type="number"
-          ></v-text-field>
-          <v-text-field
-            id="city"
-            label="Ville"
-            name="city"
-            prepend-icon="mdi-map"
-            type="text"
-          ></v-text-field>
-          <v-text-field
-            id="country"
-            label="Pays"
-            name="country"
-            prepend-icon="mdi-map"
-            type="text"
-          ></v-text-field>
-          <v-text-field
-            id="email"
-            label="Email"
-            name="email"
-            prepend-icon="mdi-email"
-            type="text"
-          ></v-text-field>
-          <v-text-field
-            id="password"
-            label="Password"
-            name="password"
-            prepend-icon="mdi-lock"
-            type="password"
           ></v-text-field>
         </v-form>
       </v-card-text>
 
       <v-row align="center" justify="center" class="pl-3">
         <v-card-actions>
-          <v-btn color="deep-purple" class="btn">Enregistrer</v-btn>
+          <v-btn class="btn">Ajouter</v-btn>
         </v-card-actions>
       </v-row>
     </v-card>
@@ -78,7 +46,7 @@ export default {};
 
 <style lang="scss" scoped>
 .container {
-  width: 20vw;
+  width: 15vw;
   height: 100%;
   color: #194C4C;
 }
