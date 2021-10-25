223494bd6492049820f97ddaec1df066c61d049b
Heranca <heranca.kamalo@gmail.com>
2021-10-25 11:36:05 +0200

    change toolbar

---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-toolbar class="white" v-if="this.$auth.loggedIn">
+    <v-toolbar-side-icon>
+      <nuxt-link to="/">
+        <img src="../static/logo-dark.png" alt="LOGO" height="100%" />
+      </nuxt-link>
+    </v-toolbar-side-icon>
+    <v-toolbar-title class="mx-0" v-text="title"></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat>Profil</v-btn>
+      <!--<v-btn flat>Blog</v-btn>-->
+      <v-btn flat class="pink lighten-3" @click="logout()">Logout</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+    methods: {
+        async logout(){
+           await this.$auth.logout();
+            alert("Successfuly Logout")
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
